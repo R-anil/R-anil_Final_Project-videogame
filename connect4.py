4949f495878292f8456059f03a834a74a2e7ba14
R-anil <126901051+R-anil@users.noreply.github.com>
2023-05-08 11:03:01 -0700

    3rd push

--- connect4.py
+++ connect4.py
@@ -1,61 +1,67 @@
 # File created by: Ryan Anil
 
-import pygame as pg
-import math
-import sys 
 import numpy as np
+import pygame as pg
+import sys
+import math
  
 BLUE = (0,0,255)
-BLACK =(0,0,0)
+BLACK = (0,0,0)
 RED = (255,0,0)
 YELLOW = (255,255,0)
-
-# dimensions
+ 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
-
-
+ 
 def create_board():
-    board = np.zeros((6,7))
+    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
-
-def drop_piece(board,row,col,piece):
-    board[row][col]= piece
-
-def is_valid_location(board,col):
-    # if this condition is true let the use drop piece.
-    # if not true col is not vacant
-    return board[5][col]==0
-
-def get_next_open_row(board,col):
-    for r in range (ROW_COUNT):
-        if board[r][col]==0:
+ 
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+ 
+def is_valid_location(board, col):
+    return board[ROW_COUNT-1][col] == 0
+ 
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
             return r
-        
+ 
 def print_board(board):
-    print(np.flip(board,0))
-
+    print(np.flip(board, 0))
+ 
 def winning_move(board, piece):
-    # Check horizontal locatons for win
+    # Check horizontal locations for win
     for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+ 
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT-3):
-            if board[r][c] == piece and board [r][c+1] == piece and board [r][c+2] == piece and board [r][c+3] == piece:
-                return True 
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+ 
     # Check positively sloped diaganols
     for c in range(COLUMN_COUNT-3):
         for r in range(ROW_COUNT-3):
-            if board[r][c] == piece and board [r+1][c+1] == piece and board [r+2][c+2] == piece and board [r+3][c+3] == piece:
-                return True 
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+ 
     # Check negatively sloped diaganols
     for c in range(COLUMN_COUNT-3):
-        for r in range(3,ROW_COUNT):
-            if board[r][c] == piece and board [r-1][c+1] == piece and board [r-2][c+2] == piece and board [r-3][c+3] == piece:
-                return True 
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+ 
 def draw_board(board):
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
-            pg.draw.rect(screen,BLUE,(c*SQUARESIZE,r*SQUARESIZE+SQUARESIZE,SQUARESIZE,SQUARESIZE))
+            pg.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
             pg.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+     
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):      
             if board[r][c] == 1:
@@ -63,34 +69,34 @@ def draw_board(board):
             elif board[r][c] == 2: 
                 pg.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
     pg.display.update()
-
-#initialize board
+ 
+ 
 board = create_board()
-#We will initialize the game_over as False.
+print_board(board)
 game_over = False
 turn = 0
-
-# initialize pygame
+ 
+#initalize pygame
 pg.init()
-
-# define screen size
+ 
+#define our screen size
 SQUARESIZE = 100
-
-# dimensions of board
-width = COLUMN_COUNT*SQUARESIZE
-height = (ROW_COUNT+1)*SQUARESIZE
-
-size = (width/height)
-
-RADIUS = int(SQUARESIZE/2-5)
-
+ 
+#define width and height of board
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+ 
+size = (width, height)
+ 
+RADIUS = int(SQUARESIZE/2 - 5)
+ 
 screen = pg.display.set_mode(size)
-# Calling Functions draw_board again
+#Calling function draw_board again
 draw_board(board)
 pg.display.update()
-
+ 
 myfont = pg.font.SysFont("arial", 75)
-
+ 
 while not game_over:
  
     for event in pg.event.get():
@@ -122,14 +128,27 @@ while not game_over:
                         label = myfont.render("Player 1 wins!!", 1, RED)
                         screen.blit(label, (40,10))
                         game_over = True
-
-    print_board(board)
-    draw_board(board)
-
-    turn += 1
-    turn = turn % 2
-
-    if game_over:
-        pg.time.wait(3000)
-
-
+ 
+ 
+            # # Ask for Player 2 Input
+            else:               
+                posx = event.pos[0]
+                col = int(math.floor(posx/SQUARESIZE))
+ 
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 2)
+ 
+                    if winning_move(board, 2):
+                        label = myfont.render("Player 2 wins!!", 1, YELLOW)
+                        screen.blit(label, (40,10))
+                        game_over = True
+ 
+            print_board(board)
+            draw_board(board)
+ 
+            turn += 1
+            turn = turn % 2
+ 
+            if game_over:
+                pg.time.wait(3000)
